8a43cae4ba0ba3e05661d22b8432cca5717085b2
David Kingman <david.kingman@gmail.com>
2013-02-25 22:34:19 -0600

    Adding classes

---
+++ src/Node.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: David
+ * Date: 2/25/13
+ * Time: 10:12 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Node {
+    private String person;
+    private User userWhoAnswered;
+}
