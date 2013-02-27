5953ded5e23b357dd7b2c7d9d66440a075a798f5
Ranny sweis <ranny.sweis@amstatz.com>
2013-02-27 14:10:08 -0600

    server stuff started

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,50 @@
-import java.io.*;
-import java.net.InetAddress;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        try {
+	        ServerSocket welcomeSocket = new ServerSocket(6001);
+	
+	        while(true) {
+		    Socket connectionSocket = null;
+			connectionSocket = welcomeSocket.accept();
+	                Connection c = new Connection(connectionSocket);
+	                Thread t = new Thread(c);
+	                t.start();
+	                askToPlay(welcomeSocket);
+	        }
+	        
+	        } catch (IOException e) {
+	            System.out.println( e + " Error");
+	            e.printStackTrace();
+	        }
+
+    }
+    
+    private static void askToPlay(ServerSocket socket){
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Would you like to play a celebrity guessing game?");
-
         // COMMENTED OUT FOR DEVELOPMENT PURPOSES
-//        try {
-//            String response = bufferedReader.readLine();
-//            if(response.toLowerCase().equals("yes")) {
-                playGame(bufferedReader, new Socket());       // replace with actual socket
-//            }
-//        } catch (IOException e) {
-//            System.out.println("Failed to read response of user");
-//            e.printStackTrace();
-//        }
+        try {
+            String response = bufferedReader.readLine();
+            if(response.toLowerCase().equals("yes")) {
+                playGame(bufferedReader, socket);       // replace with actual socket
+            }
+        } catch (IOException e) {
+            System.out.println("Failed to read response of user");
+            e.printStackTrace();
+        }
+    
     }
 
-    private static void playGame(BufferedReader bufferedReader,Socket socket) {
+    private static void playGame(BufferedReader bufferedReader,ServerSocket socket) {
 //        User user = new User(socket.getInetAddress(), "Player");
 //        NodeData nodeData = new NodeData(user);
 //        Node root = new Node(nodeData);
